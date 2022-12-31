1e3f5e1ed0ddc86bcd3c738526556fd0075e09e3
allen.w <koomoxs@gmail.com>
2022-12-31 20:42:45 +0800

    commit

--- http.go
+++ http.go
@@ -18,6 +18,25 @@ func NewProxyDialer(network, addr string) *http.Client {
 	return &http.Client{Transport: transport}
 }
 
+func HttpGetWithDialer(reqURL string, dialer *http.Client) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, reqURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+	resp, err := dialer.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request %s, ststaus %s", reqURL, resp.Status)
+	}
+
+	return io.ReadAll(resp.Body)
+}
+
 func HttpGet(reqURL string) ([]byte, error) {
 	client := &http.Client{Timeout: 5 * time.Second}
 	req, err := http.NewRequest(http.MethodGet, reqURL, nil)
