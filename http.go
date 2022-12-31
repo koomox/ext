d7f8019de498f148f5a9f615b177d3a8d4be2afd
allen.w <koomoxs@gmail.com>
2022-12-31 20:57:10 +0800

    commit

--- http.go
+++ http.go
@@ -18,8 +18,8 @@ func NewProxyDialer(network, addr string) *http.Client {
 	return &http.Client{Transport: transport}
 }
 
-func HttpGetWithDialer(reqURL string, dialer *http.Client) ([]byte, error) {
-	req, err := http.NewRequest(http.MethodGet, reqURL, nil)
+func GetWithDialer(resource string, dialer *http.Client) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, resource, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -31,47 +31,30 @@ func HttpGetWithDialer(reqURL string, dialer *http.Client) ([]byte, error) {
 
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request %s, status %s", reqURL, resp.Status)
+		return nil, fmt.Errorf("request %s, status %s", resource, resp.Status)
 	}
 
 	return io.ReadAll(resp.Body)
 }
 
-func HttpGet(reqURL string) ([]byte, error) {
-	client := &http.Client{Timeout: 5 * time.Second}
-	req, err := http.NewRequest(http.MethodGet, reqURL, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request %s, status %s", reqURL, resp.Status)
-	}
-
-	return io.ReadAll(resp.Body)
+func Get(resource string) ([]byte, error) {
+	return GetWithDialer(resource, &http.Client{Timeout: 5 * time.Second})
 }
 
-func HttpGetWithFile(reqURL, dst string) (err error) {
-	client := &http.Client{Timeout: 5 * time.Second}
-	req, err := http.NewRequest(http.MethodGet, reqURL, nil)
+func DownloadWithDialer(resource, dst string, dialer *http.Client) (err error) {
+	req, err := http.NewRequest(http.MethodGet, resource, nil)
 	if err != nil {
 		return err
 	}
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
-	resp, err := client.Do(req)
+	resp, err := dialer.Do(req)
 	if err != nil {
 		return err
 	}
 
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("request %s, status %s", reqURL, resp.Status)
+		return fmt.Errorf("request %s, status %s", resource, resp.Status)
 	}
 
 	out, err := os.Create(dst)
@@ -83,4 +66,8 @@ func HttpGetWithFile(reqURL, dst string) (err error) {
 	_, err = io.Copy(out, resp.Body)
 
 	return err
+}
+
+func Download(resource, dst string) (err error) {
+	return DownloadWithDialer(resource, dst, &http.Client{Timeout: 5 * time.Second})
 }
